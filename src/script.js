68072bb3f4f81694da8436e5e4e3a3c26b60df64
RyanantaDanendra <ryanantadanendra887@gmail.com>
2023-11-10 17:32:21 +0800

    Menambah file about dan blog

--- src/script.js
+++ src/script.js
@@ -11,3 +11,33 @@ function toggleDarkMode() {
     }
     // root.classList.add('dark-transition');
 }
+
+const projects = document.querySelectorAll('.project');
+const overlays = document.querySelectorAll('.overlay');
+// a.innerHTML = 'Click to see ';
+// const p = document.createElement('p');
+const hrefs = [
+    "https://ryanantadanendra.github.io/",
+    "https://dev-ryanantaswordpress.pantheonsite.io/",
+]
+
+projects.forEach((project, i) => {
+    project.addEventListener('mouseover', function() {
+        // overlays[i].appendChild(p);
+        // overlays[i].appendChild(a);
+        overlays[i].innerHTML = ''
+        overlays[i].innerHTML = `
+                                    <p>Made with HTML, CSS, AND JS</p>
+                                    <a href=${hrefs[i]}>Click to see!</a>
+                                `;
+        // a.href = hrefs[i];
+        // p.innerHTML = "Made with html, css, and Javascript";
+        overlays[i].style.display = 'flex';
+        overlays[i].style.flexDirection = 'column';
+        overlays[i].style.transition = 'all 2s';
+    });
+    
+    project.addEventListener('mouseout', function() {
+        overlays[i].style.display = 'none';
+    })
+})
